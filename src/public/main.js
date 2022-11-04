e21143d85fa04275c79423f62b75e097fed43473
adesewa12 <50561676+adesewa12@users.noreply.github.com>
2022-11-03 23:27:35 -0400

    main js

---
+++ src/public/main.js
@@ -0,0 +1,24 @@
+let result = document.getElementById("result");
+document.getElementById("search").addEventListener("click", () => {
+
+    fetch("/search", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            search: document.getElementById("search").value,
+        })
+    }).then((response) => {
+        if (response.status === 200) {
+            result.textContent = "Search successful";
+            result.classList.remove("error");
+        } else {
+            response.json().then((error) => {
+                result.textContent = error.error;
+                result.classList.add("error");
+            })
+        }
+    }
+    );
+});
