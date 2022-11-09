fa2990918f5228d0dc86bf238e9f6e212e938793
wc489 <wc489@drexel.edu>
2022-11-09 09:38:26 -0500

    styling update in test

--- src/public/books/books.js
+++ src/public/books/books.js
@@ -2,7 +2,6 @@ let textContainer = document.getElementById("content");
 let titleH = document.getElementById("titleH");
 let pages = 0;
 const linesPerPage = 100;
-let x;
 let currentPage = 1;
 
 // At start -> need a way to get data instead
@@ -10,7 +9,6 @@ fetch(window.location.pathname, {method : "POST"}).then((response)=>{
     // I will store things text content into local storage.
     if(response.status === 200){
         response.json().then((body) =>{
-            x =body;
             localStorage.setItem("text" , body.text);
             localStorage.setItem("title" , body.title);
             // init
@@ -37,13 +35,14 @@ function splitTextContent(){
 
     // https://stackoverflow.com/questions/3357553/how-do-i-store-an-array-in-localstorage
     for(let i = 0; i < Math.ceil(lines.length / linesPerPage); i++){
-
         localStorage.setItem(`text${i+1}`, JSON.stringify(lines.slice(i*linesPerPage, (i+1)*linesPerPage)) );
         pages += 1;
     }
 }
 
 function displayText(page, container = textContainer){
+    let title = localStorage.getItem("title");
+    titleH.textContent = `${title} (${page})`;
     let thisPageText = JSON.parse(localStorage.getItem(`text${page}`));
     textContainer.textContent = ""; // clear previous content
 
@@ -54,6 +53,12 @@ function displayText(page, container = textContainer){
     }
 }
 
+window.addEventListener('hashchange', () => {
+    let pageHash = new URL(document.URL).hash.replace("#", "");;
+    displayText(pageHash);
+    window.scrollTo(0, 0);
+});
+
 
 document.getElementById("prevPage").addEventListener("click", () => {
     if (currentPage === 1){
@@ -61,8 +66,7 @@ document.getElementById("prevPage").addEventListener("click", () => {
         // go to previous chapter
     }else{
         currentPage--;
-        displayText(currentPage);
-        window.scrollTo(0, 0);
+        updateURLhash(currentPage);
     }
     
 });
@@ -74,7 +78,10 @@ document.getElementById("nextPage").addEventListener("click", () =>{
         // go to next chapter
     }else{
         currentPage++;
-        displayText(currentPage);
-        window.scrollTo(0, 0);
+        updateURLhash(currentPage);
     }
 })
+
+function updateURLhash(page){
+    window.location.hash = String(page);
+}
